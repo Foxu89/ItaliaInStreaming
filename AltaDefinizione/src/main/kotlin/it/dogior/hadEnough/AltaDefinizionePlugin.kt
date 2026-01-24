3af78539e4c940e5f9be9fc9eccb750ddfab5197
DieGon7771 <bruttodiego6@gmail.com>
2026-01-24 22:35:01 +0100

    Update AltaDefinizionePlugin.kt

--- AltaDefinizione/src/main/kotlin/it/dogior/hadEnough/AltaDefinizionePlugin.kt
+++ AltaDefinizione/src/main/kotlin/it/dogior/hadEnough/AltaDefinizionePlugin.kt
@@ -4,42 +4,21 @@ import android.content.Context
 import android.content.SharedPreferences
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.graphics.drawable.Drawable
-import com.lagradost.cloudstream3.MainAPI
-import com.lagradost.cloudstream3.MainAPIActor
 
 @CloudstreamPlugin
 class AltaDefinizionePlugin : Plugin() {
     override fun load(context: Context) {
-        // Registra il provider principale
-        registerMainAPI(AltaDefinizioneMain(context))
-        // Registra gli estrattori
-        registerExtractorAPI(DroploadExtractor())
-    }
-}
-
-// Gestore principale che decide quale versione usare
-class AltaDefinizioneMain(private val context: Context) : MainAPIActor() {
-    private lateinit var sharedPref: SharedPreferences
-    
-    override fun getKey(): String = "altadefinizione"
-    
-    override fun getPluginName(): String = "Altadefinizione"
-    
-    override fun getPluginIcon(): Drawable? = null
-    
-    // Questo fa apparire l'icona ingranaggio nel plugin
-    override fun getSettingsView(): Class<*>? = AltaDefinizioneSettings::class.java
-    
-    override fun getMainAPI(): MainAPI {
-        // Carica le preferenze
-        sharedPref = context.getSharedPreferences("altadefinizione_prefs", Context.MODE_PRIVATE)
-        val siteVersion = sharedPref.getString("site_version", "v1") ?: "v1"
+        val prefs = context.getSharedPreferences("altadefinizione_prefs", Context.MODE_PRIVATE)
+        val version = prefs.getString("site_version", "v1") ?: "v1"
         
-        // Scegli la versione corretta
-        return when (siteVersion) {
-            "v2" -> AltaDefinizioneV2()  // Versione vecchia
-            else -> AltaDefinizioneV1()  // Versione attuale (default)
+        when (version) {
+            "v2" -> registerMainAPI(AltaDefinizioneV2())
+            else -> registerMainAPI(AltaDefinizioneV1())
         }
+    }
+    
+    // Questo abilita l'icona ingranaggio
+    override fun getSettingsView(sharedPref: SharedPreferences?): Class<*>? {
+        return AltaDefinizioneSettings::class.java
     }
 }
