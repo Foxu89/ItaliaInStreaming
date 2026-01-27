7577de89fe18bfa4c66c798dee02e676b62c8217
Foxu89 <foxcraft.20.25123@gmail.com>
2026-01-27 23:15:52 +0100

    Update StreamingCommunityPlugin.kt

--- StreamingCommunity/src/main/kotlin/it/dogior/hadEnough/StreamingCommunityPlugin.kt
+++ StreamingCommunity/src/main/kotlin/it/dogior/hadEnough/StreamingCommunityPlugin.kt
@@ -1,24 +1,27 @@
 package it.dogior.hadEnough
 
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class StreamingCommunityPlugin : Plugin() {
+    private var sharedPref: SharedPreferences? = null
+    
     override fun load(context: Context) {
-        val sharedPref = context.getSharedPreferences("streamingcommunity_prefs", Context.MODE_PRIVATE)
-        val language = sharedPref.getString("language", "it") ?: "it"
+        sharedPref = context.getSharedPreferences("StreamingCommunity", Context.MODE_PRIVATE)
+        val language = sharedPref?.getString("language", "it") ?: "it"
         
-        // Registra il plugin corretto in base alla lingua
         when (language) {
             "en" -> registerMainAPI(StreamingCommunityEN())
-            else -> registerMainAPI(StreamingCommunityIT())  // Default italiano
+            else -> registerMainAPI(StreamingCommunityIT())
         }
+        
+        registerExtractorAPI(VixCloudExtractor())
+        registerExtractorAPI(VixSrcExtractor())
 
-        // Abilita le impostazioni (ICONA INGRANAGGIO)
         openSettings = { ctx ->
             val activity = ctx as AppCompatActivity
             val frag = StreamingCommunitySettings(this, sharedPref)
