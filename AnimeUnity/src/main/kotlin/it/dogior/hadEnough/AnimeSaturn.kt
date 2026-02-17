db600e23b6ab980a9341d7b54c79bf60cde47099
Foxu89 <foxcraft.20.25123@gmail.com>
2026-02-17 23:46:32 +0100

    Update AnimeSaturn.kt

--- AnimeUnity/src/main/kotlin/it/dogior/hadEnough/AnimeSaturn.kt
+++ AnimeUnity/src/main/kotlin/it/dogior/hadEnough/AnimeSaturn.kt
@@ -25,6 +25,7 @@ class AnimeSaturn : MainAPI() {
         "$mainUrl/toplist" to "Top Anime",
         "$mainUrl/animeincorso" to "Anime in Corso",
         "$mainUrl/newest" to "Nuove Aggiunte",
+        "$mainUrl/upcoming" to "In Arrivo...",
     )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
