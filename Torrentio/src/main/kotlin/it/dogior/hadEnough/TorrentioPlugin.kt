59a78f42412daa526e38b16966721729667b8b59
Foxu89 <255663099+Foxu89@users.noreply.github.com>
2026-04-02 22:27:19 +0200

    Update TorrentioPlugin.kt

--- Torrentio/src/main/kotlin/it/dogior/hadEnough/TorrentioPlugin.kt
+++ Torrentio/src/main/kotlin/it/dogior/hadEnough/TorrentioPlugin.kt
@@ -3,11 +3,20 @@ package it.dogior.hadEnough
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
 
 @CloudstreamPlugin
-class TorrentioPlugin: Plugin() {
+class TorrentioPlugin : Plugin() {
     override fun load(context: Context) {
-        // All providers should be added in this manner
         registerMainAPI(Torrentio())
+        
+        // Aggiungi impostazioni
+        openSettings = { ctx ->
+            val activity = ctx as? AppCompatActivity
+            if (activity != null) {
+                val frag = TorrentioSettings(this)
+                frag.show(activity.supportFragmentManager, "TorrentioSettings")
+            }
+        }
     }
 }
