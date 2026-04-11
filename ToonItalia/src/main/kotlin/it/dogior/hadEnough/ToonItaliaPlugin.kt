caad6f857b6b00384d7e25639b14991f408844f3
Foxu89 <255663099+Foxu89@users.noreply.github.com>
2026-04-11 12:33:52 +0200

    Update ToonItaliaPlugin.kt

--- ToonItalia/src/main/kotlin/it/dogior/hadEnough/ToonItaliaPlugin.kt
+++ ToonItalia/src/main/kotlin/it/dogior/hadEnough/ToonItaliaPlugin.kt
@@ -3,15 +3,15 @@ package it.dogior.hadEnough
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
-import it.dogior.hadEnough.extractors.MaxStreamExtractor
-import it.dogior.hadEnough.extractors.StreamTapeExtractor
+import it.dogior.hadEnough.extractors.VOEExtractor
 
 @CloudstreamPlugin
 class ToonItaliaPlugin: Plugin() {
     override fun load(context: Context) {
-        // All providers should be added in this manner
+        // Registra il main API
         registerMainAPI(ToonItalia())
-        registerExtractorAPI(StreamTapeExtractor())
-        registerExtractorAPI(MaxStreamExtractor())
+        
+        // Registra l'estrattore VOE
+        registerExtractorAPI(VOEExtractor())
     }
 }
