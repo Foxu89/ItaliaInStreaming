f42fc407001bab405eb541c7657fc0adb2031d7d
Foxu89 <255663099+Foxu89@users.noreply.github.com>
2026-04-26 21:30:04 +0200

    Update build.gradle.kts

--- StreamITA/build.gradle.kts
+++ StreamITA/build.gradle.kts
@@ -16,7 +16,7 @@ cloudstream {
         "Cartoon"
     )
     
-    requiresResources = true
+    requiresResources = false
     iconUrl = "https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea11890a74b3666c911d92.svg"
 }
 
