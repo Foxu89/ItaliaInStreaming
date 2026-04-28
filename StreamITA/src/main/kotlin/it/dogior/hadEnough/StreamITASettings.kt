91f26f109f6a3f597d5b2ca24117f1e43f4e5bff
Foxu89 <255663099+Foxu89@users.noreply.github.com>
2026-04-28 16:34:52 +0200

    Update StreamITASettings.kt

--- StreamITA/src/main/kotlin/it/dogior/hadEnough/StreamITASettings.kt
+++ StreamITA/src/main/kotlin/it/dogior/hadEnough/StreamITASettings.kt
@@ -9,11 +9,15 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class StreamITASettings(
-    private val plugin: StreamITAPlugin
-) : BottomSheetDialogFragment() {
+abstract class StreamITABaseSettingsFragment : BottomSheetDialogFragment() {
 
-    private val res = plugin.resources ?: error("Resources not available")
+    protected val plugin: StreamITAPlugin
+        get() = StreamITAPlugin.activePlugin ?: error("Plugin not available")
+
+    protected val res
+        get() = plugin.resources ?: error("Resources not available")
+
+    protected abstract val layoutName: String
 
     override fun onStart() {
         super.onStart()
@@ -27,16 +31,65 @@ class StreamITASettings(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val layoutId = res.getIdentifier("settings_streamita", "layout", BuildConfig.LIBRARY_PACKAGE_NAME)
+    ): View? {
+        val layoutId = res.getIdentifier(layoutName, "layout", BuildConfig.LIBRARY_PACKAGE_NAME)
         return inflater.inflate(res.getLayout(layoutId), container, false)
     }
+
+    protected fun getStringRes(name: String): String {
+        val id = res.getIdentifier(name, "string", BuildConfig.LIBRARY_PACKAGE_NAME)
+        return if (id != 0) res.getString(id) else ""
+    }
+
+    protected fun <T : View> View.findViewByName(name: String): T? {
+        val id = res.getIdentifier(name, "id", BuildConfig.LIBRARY_PACKAGE_NAME)
+        return findViewById(id)
+    }
+}
+
+class StreamITASettings : StreamITABaseSettingsFragment() {
+
+    override val layoutName: String = "settings_streamita"
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Build info
         val buildInfoId = res.getIdentifier("header_build_info", "id", BuildConfig.LIBRARY_PACKAGE_NAME)
         view.findViewById<TextView>(buildInfoId)?.text =
             "Ultimo aggiornamento: ${BuildConfig.BUILD_COMPLETED_AT_ROME}"
+
+        // Card click listeners
+        view.findViewByName<View>("general_settings_card")?.setOnClickListener {
+            StreamITAGeneralSettings().show(parentFragmentManager, "GeneralSettings")
+        }
+
+        view.findViewByName<View>("extractors_settings_card")?.setOnClickListener {
+            StreamITAExtractorsSettings().show(parentFragmentManager, "ExtractorsSettings")
+        }
+
+        view.findViewByName<View>("ui_settings_card")?.setOnClickListener {
+            StreamITAUISettings().show(parentFragmentManager, "UISettings")
+        }
+
+        view.findViewByName<View>("advanced_settings_card")?.setOnClickListener {
+            StreamITAAdvancedSettings().show(parentFragmentManager, "AdvancedSettings")
+        }
     }
+}
+
+class StreamITAGeneralSettings : StreamITABaseSettingsFragment() {
+    override val layoutName: String = "settings_streamita_general"
+}
+
+class StreamITAExtractorsSettings : StreamITABaseSettingsFragment() {
+    override val layoutName: String = "settings_streamita_extractors"
+}
+
+class StreamITAUISettings : StreamITABaseSettingsFragment() {
+    override val layoutName: String = "settings_streamita_ui"
+}
+
+class StreamITAAdvancedSettings : StreamITABaseSettingsFragment() {
+    override val layoutName: String = "settings_streamita_advanced"
 }
