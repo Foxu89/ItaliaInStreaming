84ecba10885aa23e47b5bd07082138db45fd3326
Foxu89 <255663099+Foxu89@users.noreply.github.com>
2026-04-28 19:00:42 +0200

    Update StreamITAPlugin.kt

--- StreamITA/src/main/kotlin/it/dogior/hadEnough/StreamITAPlugin.kt
+++ StreamITA/src/main/kotlin/it/dogior/hadEnough/StreamITAPlugin.kt
@@ -1,6 +1,7 @@
 package it.dogior.hadEnough
 
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
@@ -9,15 +10,23 @@ import com.lagradost.cloudstream3.plugins.Plugin
 class StreamITAPlugin : Plugin() {
     companion object {
         internal var activePlugin: StreamITAPlugin? = null
+        internal var activeSharedPref: SharedPreferences? = null
+
+        const val PREF_LANG = "lang"
+        const val PREF_LANG_POSITION = "lang_position"
     }
 
     override fun load(context: Context) {
+        val sharedPref = context.getSharedPreferences("StreamITA", Context.MODE_PRIVATE)
         activePlugin = this
+        activeSharedPref = sharedPref
+
         registerMainAPI(StreamITA())
 
         openSettings = { ctx ->
             val activity = ctx as AppCompatActivity
             activePlugin = this
+            activeSharedPref = sharedPref
             StreamITASettings().show(activity.supportFragmentManager, "StreamITASettings")
         }
     }
