372e22820344ed5da864fea051e61d11a903c4e6
Foxu89 <255663099+Foxu89@users.noreply.github.com>
2026-04-28 15:59:09 +0200

    Update StreamITASettings.kt

--- StreamITA/src/main/kotlin/it/dogior/hadEnough/StreamITASettings.kt
+++ StreamITA/src/main/kotlin/it/dogior/hadEnough/StreamITASettings.kt
@@ -1,6 +1,5 @@
 package it.dogior.hadEnough
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +9,11 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class StreamITASettings : BottomSheetDialogFragment() {
+class StreamITASettings(
+    private val plugin: StreamITAPlugin
+) : BottomSheetDialogFragment() {
+
+    private val res = plugin.resources ?: error("Resources not available")
 
     override fun onStart() {
         super.onStart()
@@ -25,13 +28,15 @@ class StreamITASettings : BottomSheetDialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.settings_streamita, container, false)
+        val layoutId = res.getIdentifier("settings_streamita", "layout", BuildConfig.LIBRARY_PACKAGE_NAME)
+        return inflater.inflate(res.getLayout(layoutId), container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<TextView>(R.id.build_info_text)?.text =
+        val buildInfoId = res.getIdentifier("header_build_info", "id", BuildConfig.LIBRARY_PACKAGE_NAME)
+        view.findViewById<TextView>(buildInfoId)?.text =
             "Ultimo aggiornamento: ${BuildConfig.BUILD_COMPLETED_AT_ROME}"
     }
 }
