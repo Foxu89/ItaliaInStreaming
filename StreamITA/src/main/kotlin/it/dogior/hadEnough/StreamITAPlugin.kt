965c0435c6d070d232648068e6bf49a4b829b1ef
Foxu89 <255663099+Foxu89@users.noreply.github.com>
2026-04-28 15:45:10 +0200

    Update StreamITAPlugin.kt

--- StreamITA/src/main/kotlin/it/dogior/hadEnough/StreamITAPlugin.kt
+++ StreamITA/src/main/kotlin/it/dogior/hadEnough/StreamITAPlugin.kt
@@ -1,13 +1,18 @@
 package it.dogior.hadEnough
 
 import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class StreamITAPlugin : Plugin() {
     override fun load(context: Context) {
-        // Registra il provider principale
         registerMainAPI(StreamITA())
+
+        openSettings = { ctx ->
+            val activity = ctx as AppCompatActivity
+            StreamITASettings().show(activity.supportFragmentManager, "StreamITASettings")
+        }
     }
 }
