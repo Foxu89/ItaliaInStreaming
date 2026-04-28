d61461c612412af07607c2c9b5590ba101d7faa5
Foxu89 <255663099+Foxu89@users.noreply.github.com>
2026-04-28 15:44:16 +0200

    Create StreamITASettings.kt

---
+++ StreamITA/src/main/kotlin/it/dogior/hadEnough/StreamITASettings.kt
@@ -0,0 +1,37 @@
+package it.dogior.hadEnough
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class StreamITASettings : BottomSheetDialogFragment() {
+
+    override fun onStart() {
+        super.onStart()
+        (dialog as? BottomSheetDialog)?.behavior?.apply {
+            state = BottomSheetBehavior.STATE_EXPANDED
+            skipCollapsed = true
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.settings_streamita, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.build_info_text)?.text =
+            "Ultimo aggiornamento: ${BuildConfig.BUILD_COMPLETED_AT_ROME}"
+    }
+}
