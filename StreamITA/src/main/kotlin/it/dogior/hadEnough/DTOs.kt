e305504a5b73517a17931039e134d38781c557e9
Foxu89 <255663099+Foxu89@users.noreply.github.com>
2026-05-01 08:12:54 +0200

    Update DTOs.kt

--- StreamITA/src/main/kotlin/it/dogior/hadEnough/DTOs.kt
+++ StreamITA/src/main/kotlin/it/dogior/hadEnough/DTOs.kt
@@ -27,7 +27,7 @@ data class LinkData(
     val isMovie: Boolean = false,
     val episodeTitle: String? = null,
     val episodeOverview: String? = null,
-    val streamingCommunityIframeUrl: String? = null,
+    val scIframeUrl: String? = null,
 )
 
 data class MediaDetail(
