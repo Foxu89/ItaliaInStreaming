0519003b8848f5e36eca10e382f6925c2404f03e
Foxu89 <255663099+Foxu89@users.noreply.github.com>
2026-05-02 20:32:57 +0200

    Update build.gradle.kts

--- SectionOrganizer/build.gradle.kts
+++ SectionOrganizer/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     implementation("com.google.android.material:material:1.13.0")
 }
 
-version = 3
+version = 4
 
 cloudstream {
     description = "Organizza homepage tra vari plugin"
