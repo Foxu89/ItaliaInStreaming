f936861ee07d5d37976ab5a873a28eb88ccb7b7c
Foxu89 <255663099+Foxu89@users.noreply.github.com>
2026-08-13 22:03:01 +0200

    Update build.gradle.kts

--- DiscordRPC/build.gradle.kts
+++ DiscordRPC/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     compileOnly("com.jaredrummler:colorpicker:1.1.0") // MainActivity implementa ColorPickerDialogListener
 }
 
-version = 7
+version = 8
 
 android {
     buildFeatures {
